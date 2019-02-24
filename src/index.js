c499cfe2aec3f5a2eb00efa60896ae1a7469d98a
andre <andre@pop-os.localdomain>
2019-02-24 10:59:32 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//Route controller
+
+import path from "path";
+import globby from "globby";
+import express from "express";
+
+export default async (routesPath) => {
+    const routesPathResolve = path.join(process.cwd(), routesPath);
+
+    const router = express.Router();
+
+    await globby([`${routesPathResolve}/*.js`, `${routesPathResolve}/**/*.js`]).then((paths) => {
+        paths.forEach((routePath) => {
+            var routeRequest = require(path.resolve(routePath));
+
+            if(typeof routeRequest == "function")
+                routeRequest(router);
+            else if(typeof routeRequest.default == "function")
+                routeRequest.default(router);
+        });
+
+        return true;
+    });
+
+    return router;
+}
